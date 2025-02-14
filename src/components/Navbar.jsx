2695b7dac8bd164aae037220aeb706217f48210a
chloeonsol <chloeonsol@gmail.com>
2025-02-14 13:51:29 +0100

    Descripción de los cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,19 +14,19 @@ export default function Navbar() {
 
         {/* Iconos a la derecha */}
         <div className="navbar-right">
-          <a href="https://t.me/yourtelegram" target="_blank" rel="noopener noreferrer" className="social-button">
+          <a href="https://t.me/fartcupidsol" target="_blank" rel="noopener noreferrer" className="social-button">
             <img src="/icons/telegram.svg" alt="" />
           </a>
-          <a href="https://x.com/yourprofile" target="_blank" rel="noopener noreferrer" className="social-button">
+          <a href="https://x.com/fartcupidsol" target="_blank" rel="noopener noreferrer" className="social-button">
             <img src="/icons/x.svg" alt="" />
           </a>
-          <a href="https://dexscreener.com/" target="_blank" rel="noopener noreferrer" className="social-button">
+          <a href="https://dexscreener.com/solana/ea6mpzcvkd8yvcbwwj4rhugi8jyy3kvihwxkzfqspysk" target="_blank" rel="noopener noreferrer" className="social-button">
             <img src="/icons/dexscreener.svg" alt="" />
           </a>
-          <a href="https://www.dextools.io/" target="_blank" rel="noopener noreferrer" className="social-button">
+          <a href="https://www.dextools.io/app/es/solana/pair-explorer/6D6hWRumqdRA4oeFUj2zaybTSpDqtrKuTjZXoqCbpump?t=1739537463720" target="_blank" rel="noopener noreferrer" className="social-button">
             <img src="/icons/dextools.svg" alt="" />
           </a>
-          <a href="https://www.jup.ag/" target="_blank" rel="noopener noreferrer" className="btn-buy">
+          <a href="https://pump.fun/coin/6D6hWRumqdRA4oeFUj2zaybTSpDqtrKuTjZXoqCbpump" target="_blank" rel="noopener noreferrer" className="btn-buy">
           BUY
         </a>
         </div>
